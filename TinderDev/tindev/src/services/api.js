6ecf4656e90322e44012cd7975fb713a509f4763
Marcelo De Lucca Costa Da Ponte <marcelo.ponte@metalsa.com>
2020-05-18 13:00:54 -0300

    Deletando Tindev

--- TinderDev/tindev/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-
-const api = axios.create({
-    baseURL: 'http://10.44.28.71:3333'
-    // usar comando adb reverse tcp:3333 tcp:3333 ou ip da maquina
-})
-
-export default api
